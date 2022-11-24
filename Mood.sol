cceeebb3bd0a214af0dda7846dde47550b3ad430
Pravar Agarwal <pravar20229@iiitd.ac.in>
2022-11-24 20:21:08 +0530

    fix: Sol file added

---
+++ Mood.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.1;
+
+contract MoodDiary{
+    string mood;
+    
+        function setMood(string memory _mood) public{
+        mood = _mood;
+    }
+    
+    function getMood() public view returns(string memory){
+        return mood;
+    }
+}
